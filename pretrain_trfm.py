e0ff472d16cb12f910af7cafb299e38391716911
alex mueller <alex@endogena.com>
2020-02-19 08:46:35 +0100

    combined dataset and vocab in utils and simplified

--- pretrain_trfm.py
+++ pretrain_trfm.py
@@ -3,7 +3,6 @@ import math
 import os
 
 import numpy as np
-import pandas as pd
 import torch
 from torch.nn import Module, Dropout, Embedding, Linear, Transformer
 from torch import optim
@@ -11,8 +10,7 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from build_vocab import WordVocab
-from dataset import Seq2seqDataset
+from utils import WordVocab, Seq2seqDataset, split
 
 
 class PositionalEncoding(Module):
@@ -33,13 +31,13 @@ class PositionalEncoding(Module):
 
 
 class TrfmSmiles(Module):
-    def __init__(self, in_size, hidden_size, out_size, n_layers, dropout=0.1):
+    def __init__(self, in_size, hidden_size, out_size, n_layers, nhead=4, dropout=0.1):
         super(TrfmSmiles, self).__init__()
         self.in_size = in_size
         self.hidden_size = hidden_size
         self.embed = Embedding(in_size, hidden_size)
         self.pe = PositionalEncoding(hidden_size, dropout)
-        self.trfm = Transformer(d_model=hidden_size, nhead=4, num_encoder_layers=n_layers,
+        self.trfm = Transformer(d_model=hidden_size, nhead=nhead, num_encoder_layers=n_layers,
                                 num_decoder_layers=n_layers, dim_feedforward=hidden_size)
         self.out = Linear(hidden_size, out_size)
 
@@ -98,13 +96,12 @@ def evaluate(model, test_loader, vocab):
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Hyperparams')
     parser.add_argument('--n_epoch', '-e', type=int, default=5, help='number of epochs')
-    parser.add_argument('--vocab', '-v', type=str, default='.data/vocab.pkl', help='vocabulary (.pkl)')
-    parser.add_argument('--data', '-d', type=str, default='.data/chembl24_corpus.txt', help='train corpus')
+    parser.add_argument('--data', '-d', type=str, default='.data/chembl24.txt', help='train corpus')
     parser.add_argument('--out-dir', '-o', type=str, default='.save', help='output directory')
     parser.add_argument('--name', '-n', type=str, default='ST', help='model name')
     parser.add_argument('--seq_len', type=int, default=220, help='maximum length of the paired seqence')
     parser.add_argument('--batch_size', '-b', type=int, default=32, help='batch size')
-    parser.add_argument('--n_worker', '-w', type=int, default=16, help='number of workers')
+    parser.add_argument('--n_worker', '-w', type=int, default=8, help='number of workers')
     parser.add_argument('--hidden', type=int, default=128, help='length of hidden vector')
     parser.add_argument('--n_layer', '-l', type=int, default=3, help='number of layers')
     parser.add_argument('--n_head', type=int, default=4, help='number of attention heads')
@@ -118,23 +115,41 @@ def main():
     args = parse_arguments()
     assert torch.cuda.is_available()
 
-    print('Loading dataset...')
-    vocab = WordVocab.load_vocab(args.vocab)
-    dataset = Seq2seqDataset(pd.read_csv(args.data)['canonical_smiles'].values, vocab)
+    print('\nLoading and preparing dataset...')
+    with open(args.data, "r") as f:
+        text = list()
+        smls = list()
+        for line in tqdm(f):
+            sl = split(line)
+            text.append(sl)
+            if len(sl) <= (args.seq_len - 2):
+                smls.append(line)
+        vocab = WordVocab(text, min_freq=5)
+    vocab.save_vocab('.data/vocab.pkl')
+    print("\nVocab size: %d" % len(vocab))
+    print(vocab.stoi)
+    print("\n%d examples are comply with seq_len of %d" % (len(smls), args.seq_len))
+
+    print("\nSplitting dataset into test and train...")
+    dataset = Seq2seqDataset(smls, vocab, seq_len=args.seq_len)
     train, test = torch.utils.data.random_split(dataset, [len(dataset) - int(len(dataset) * args.test_size),
                                                           int(len(dataset) * args.test_size)])
     train_loader = DataLoader(train, batch_size=args.batch_size, shuffle=True, num_workers=args.n_worker)
     test_loader = DataLoader(test, batch_size=args.batch_size, shuffle=False, num_workers=args.n_worker)
-    print('Train size:', len(train))
+    print('\nTrain size:', len(train))
     print('Test size:', len(test))
     del dataset, train, test
-    model = TrfmSmiles(len(vocab), args.hidden, len(vocab), args.n_layer).cuda()
+
+    print("\nBuilding model...")
+    model = TrfmSmiles(len(vocab), args.hidden, len(vocab), args.n_layer, nhead=args.n_head).cuda()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     print(model)
-    print('Total parameters:', sum(p.numel() for p in model.parameters()))
+    print('\nTotal parameters:', sum(p.numel() for p in model.parameters()))
 
+    print("\nTraining...")
     best_loss = None
-    for epoch in range(1, args.n_epoch):
+    for epoch in range(1, 5):
+        print("\n----- Epoch %d -----" % epoch)
         for batch, sm in tqdm(enumerate(train_loader)):
             sm = torch.t(sm.cuda())  # (T,B)
             optimizer.zero_grad()
@@ -143,17 +158,17 @@ def main():
             loss.backward()
             optimizer.step()
             if batch % 1000 == 0:
-                print('Train {:3d}: iter {:5d} | loss {:.4f} | ppl {:.4f}'.format(epoch, batch, loss.item(),
-                                                                                  math.exp(loss.item())))
+                print(' Train Ep.{:3d}: iter {:5d} | loss {:.4f} | ppl {:.4f}'.format(epoch, batch, loss.item(),
+                                                                                      math.exp(loss.item())))
             if batch % 10000 == 0:
                 loss = evaluate(model, test_loader, vocab)
-                print('Val {:3d}: iter {:5d} | loss {:.4f} | ppl {:.4f}'.format(epoch, batch, loss, math.exp(loss)))
+                print(' Val Ep.{:3d}: iter {:5d} | loss {:.4f} | ppl {:.4f}'.format(epoch, batch, loss, math.exp(loss)))
                 # Save the model if the validation loss is the best we've seen so far.
                 if not best_loss or loss < best_loss:
                     print("[!] saving model...")
                     if not os.path.isdir("./.save"):
                         os.makedirs("./.save")
-                    torch.save(model.state_dict(), './.save/trfm_new_%d_%d.pkl' % (epoch, batch))
+                    torch.save(model.state_dict(), './.save/trfm_%d_%d.pkl' % (epoch, batch))
                     best_loss = loss
 
 
--- utils.py
+++ utils.py
@@ -1,7 +1,185 @@
 import re
+import pickle
+import torch
+import numpy as np
+from collections import Counter
+from torch.utils.data import Dataset
+from rdkit import Chem
+from tqdm import tqdm
 
 
 def split(smiles):
     pattern = r'\^|\s|#|=|-[0-9]*|\+[0-9]*|[0-9]|\[.{2,5}\]|%[0-9]{2}|\(|\)|\.|/|\\|:|@+|\{|\}|Cl|Ca|Cu|Br|Be|Ba|Bi|' \
               'Si|Se|Sr|Na|Ni|Rb|Ra|Xe|Li|Al|As|Ag|Au|Mg|Mn|Te|Zn|He|Kr|Fe|[BCFHIKNOPScnos]'
     return ' '.join(re.findall(pattern, smiles))
+
+
+class TorchVocab(object):
+    def __init__(self, counter, max_size=None, min_freq=1, specials=None,
+                 vectors=None, unk_init=None, vectors_cache=None):
+
+        self.freqs = counter
+        counter = counter.copy()
+        min_freq = max(min_freq, 1)
+
+        if not specials:
+            specials = [' ', '<oov>']
+        self.itos = list(specials)
+        for tok in specials:
+            del counter[tok]
+
+        max_size = None if max_size is None else max_size + len(self.itos)
+
+        words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+
+        for word, freq in words_and_frequencies:
+            if freq < min_freq or len(self.itos) == max_size:
+                break
+            self.itos.append(word)
+
+        self.stoi = {tok: i for i, tok in enumerate(self.itos)}
+
+        self.vectors = None
+        if vectors is not None:
+            self.load_vectors(vectors, unk_init=unk_init, cache=vectors_cache)
+        else:
+            assert unk_init is None and vectors_cache is None
+
+    def __eq__(self, other):
+        if self.freqs != other.freqs:
+            return False
+        if self.stoi != other.stoi:
+            return False
+        if self.itos != other.itos:
+            return False
+        if self.vectors != other.vectors:
+            return False
+        return True
+
+    def __len__(self):
+        return len(self.itos)
+
+    def vocab_rerank(self):
+        self.stoi = {word: i for i, word in enumerate(self.itos)}
+
+    def extend(self, v, sort=False):
+        words = sorted(v.itos) if sort else v.itos
+        for w in words:
+            if w not in self.stoi:
+                self.itos.append(w)
+                self.stoi[w] = len(self.itos) - 1
+
+
+class Vocab(TorchVocab):
+    def __init__(self, counter, max_size=None, min_freq=1):
+        self.pad_index = 0
+        self.unk_index = 1
+        self.eos_index = 2
+        self.sos_index = 3
+        self.mask_index = 4
+        super().__init__(counter, specials=[" ", "<unk>", "<eos>", "^", "<mask>"], max_size=max_size,
+                         min_freq=min_freq)
+
+    def to_seq(self, sentece, seq_len, with_eos=False, with_sos=False) -> list:
+        pass
+
+    def from_seq(self, seq, join=False, with_pad=False):
+        pass
+
+    @staticmethod
+    def load_vocab(vocab_path: str) -> 'Vocab':
+        with open(vocab_path, "rb") as f:
+            return pickle.load(f)
+
+    def save_vocab(self, vocab_path):
+        with open(vocab_path, "wb") as f:
+            pickle.dump(self, f)
+
+
+class WordVocab(Vocab):
+    def __init__(self, texts, max_size=None, min_freq=1):
+        print("\nBuilding vocab...")
+        counter = Counter()
+        for line in tqdm(texts):
+            if isinstance(line, list):
+                words = line
+            else:
+                words = line.replace("\n", "").replace("\t", "").split()
+
+            for word in words:
+                counter[word] += 1
+        super().__init__(counter, max_size=max_size, min_freq=min_freq)
+
+    def to_seq(self, sentence, seq_len=None, with_eos=False, with_sos=False, with_len=False):
+        if isinstance(sentence, str):
+            sentence = sentence.split()
+
+        seq = [self.stoi.get(word, self.unk_index) for word in sentence]
+
+        if with_eos:
+            seq += [self.eos_index]  # this would be index 1
+        if with_sos:
+            seq = [self.sos_index] + seq
+
+        origin_seq_len = len(seq)
+
+        if seq_len is None:
+            pass
+        elif len(seq) <= seq_len:
+            seq += [self.pad_index for _ in range(seq_len - len(seq))]
+        else:
+            seq = seq[:seq_len]
+
+        return (seq, origin_seq_len) if with_len else seq
+
+    def from_seq(self, seq, join=False, with_pad=False):
+        words = [self.itos[idx] if idx < len(self.itos) else "<%d>" % idx for idx in seq if not with_pad or idx != self.pad_index]
+        return " ".join(words) if join else words
+
+    @staticmethod
+    def load_vocab(vocab_path: str) -> 'WordVocab':
+        with open(vocab_path, "rb") as f:
+            return pickle.load(f)
+
+
+class SmilesEnumerator(object):
+    def __init__(self, isomeric=True, canonical=False):
+        self.isomericSmiles = isomeric
+        self.canonical = canonical
+
+    def randomize_smiles(self, smiles):
+        m = Chem.MolFromSmiles(smiles)
+        if m is None:
+            return None
+        ans = list(range(m.GetNumAtoms()))
+        np.random.shuffle(ans)
+        nm = Chem.RenumberAtoms(m, ans)
+        return Chem.MolToSmiles(nm, canonical=self.canonical, isomericSmiles=self.isomericSmiles)
+
+    def __call__(self, smiles):
+        sm_r = self.randomize_smiles(smiles)
+        if sm_r is None:
+            sm_spaced = split(smiles)
+        else:
+            sm_spaced = split(sm_r)
+        return sm_spaced.split()
+
+
+class Seq2seqDataset(Dataset):
+    def __init__(self, smiles, vocab, seq_len=200):
+        self.smiles = smiles
+        self.vocab = vocab
+        self.seq_len = seq_len
+        self.transform = SmilesEnumerator()
+
+    def __len__(self):
+        return len(self.smiles)
+
+    def __getitem__(self, item):
+        sm = self.transform(self.smiles[item])  # List
+        content = [self.vocab.stoi.get(token, self.vocab.unk_index) for token in sm]
+        X = [self.vocab.sos_index] + content + [self.vocab.eos_index]
+        padding = [self.vocab.pad_index] * (self.seq_len - len(X))
+        X.extend(padding)
+        return torch.tensor(X)
